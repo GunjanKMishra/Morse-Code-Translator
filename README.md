d56eb9874e84a145c19d307c5c3d1992e46cb51b
Gunjan Mishra <gunjanmishra096@gmail.com>
2024-02-24 18:01:52 +0530

    readme and minor update.

---
+++ README.md
@@ -0,0 +1,45 @@
+# Morse Code Translator
+
+This project is a simple web application that allows users to translate English text into Morse code.
+
+## Table of Contents
+
+- [About](#about)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Demo](#demo)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Translate English text to Morse code.
+- Simple and intuitive user interface.
+- Responsive design, suitable for desktop and mobile devices.
+
+## Technologies Used
+
+- HTML
+- CSS (with Tailwind CSS framework)
+- JavaScript
+
+## How to Use
+
+1. Clone the repository to your local machine.
+2. Run the ```npm i```to install the node_modules.
+3. Open the `index.html` file in your web browser.
+4. Enter the text you want to translate in the appropriate input box.
+
+## Demo
+
+You can find a live demo of this project [here](https://gunjankmishra.github.io/Morse-Code-Translator/).
+
+## Contributing
+
+Contributions are welcome! If you find any bugs or have suggestions for improvements, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ script.js
@@ -0,0 +1,26 @@
+const MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....', 'i': '..', 'j': '.---',
+    'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.', 's': '...', 't': '-',
+    'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    ',': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': '/'
+}
+
+
+const englishTextArea = document.querySelector("#englishBox");
+const morseTextArea = document.querySelector("#morseBox");
+
+
+englishTextArea.addEventListener("input",()=>{
+    console.log("CHANGE");
+    const englishArray = englishTextArea.value.split('');
+    let morseArray = englishArray.map((value)=>{
+        return MORSE_CODE_DICT[value];
+    });
+    let morseText = morseArray.join('   ');
+    console.log(morseText);
+    morseTextArea.innerHTML = morseText;
+});
